d1619c24fb191f9efd2703d088ed983f6108bcc6
kimilguk <boramcom@daum.net>
2023-04-21 16:30:04 +0900

    스웨거 API에서 Authorization 토큰값 추가해서 테스트 할 수 있도록 코드 추가

--- src/main/java/egovframework/com/config/SwaggerConfig.java
+++ src/main/java/egovframework/com/config/SwaggerConfig.java
@@ -11,6 +11,11 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+// accessToken 입력 화면과 처리 라이브러리 추가(아래4줄)
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.service.*;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -27,9 +32,32 @@ public class SwaggerConfig {
 				.select()
 				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any())
-				.build();	
+				.build()
+				.securityContexts(Arrays.asList(securityContext())) // 스웨그에서 컨텐츠 url 접근 시 인증처리를 위한 보안 규칙 호출
+                .securitySchemes(Arrays.asList(apiKey())); // 스웨그 화면상단에 토큰값 입력하는 창 구조 호출
 	}
-	
+	// accessToken 입력 화면 구조 
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+    // 스웨그에서 컨텐츠 url 접근 시 인증처리를 위한 보안 규칙 추가(아래)
+    private SecurityContext securityContext() {
+        return springfox
+                .documentation
+                .spi.service
+                .contexts
+                .SecurityContext
+                .builder()
+                .securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
+    }
+    // 토큰 인증영역 배열을 반환하는 매서드
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything"); // 인증영역 객체 생성
+        AuthorizationScope[] authorizationScopeArray = new AuthorizationScope[1]; // 토큰 배열변수 선언
+        authorizationScopeArray[0] = authorizationScope; // 토큰 배열변수에 인증영역 지정
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopeArray));
+    }
+    
 	public ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
 				.title(API_NAME)
